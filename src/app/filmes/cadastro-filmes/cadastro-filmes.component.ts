cb52950206ddff860d54067e4cba6053f4155c6c
Glauco Todesco <glauco.todesco@gmail.com>
2021-06-23 15:56:55 -0300

    Aula04-Criando um formulario

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { allowedNodeEnvironmentFlags } from 'process';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -8,17 +9,36 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class CadastroFilmesComponent implements OnInit {
 
-  options: FormGroup;
+  cadastro: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
 
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
+    this.cadastro = this.fb.group({
+        titulo:  ['', [Validators.required,Validators.minLength(2), Validators.maxLength(256)]],
+        urlFoto: ['', [Validators.minLength(10)]],
+        dtLancamento: ['',[Validators.required]],
+        descricao: [''],
+        nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+        urlIMDb: ['', [Validators.minLength(10)]],
+        genero: ['', [Validators.required]]
     });
 
+  }
+
+  reinicarForm(): void {
+    this.cadastro.reset;
+  }
+
+  salvar(): void {
+    if(this.cadastro.invalid){
+      alert('Invalido\n' );
+      return;
+    }
+    else{
+      alert('Sucesso\n ' + JSON.stringify(this.cadastro.value, null,4));
+    }
   }
 
 }
