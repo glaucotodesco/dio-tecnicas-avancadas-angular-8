0a1808be7a14fc8f12b27f35ec31ae063e2d8f6c
Glauco Todesco <glauco.todesco@gmail.com>
2021-06-29 18:50:11 -0300

    Chapter 05 - Finished

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,7 +1,13 @@
+import { Alerta } from './../../shared/models/alerta';
+import { AlertaComponent } from './../../shared/components/alerta/alerta.component';
+import { FilmesService } from './../../core/filmes.service';
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { allowedNodeEnvironmentFlags } from 'process';
 import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
+import { Filme } from 'src/app/shared/models/filme';
+import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -13,8 +19,13 @@ export class CadastroFilmesComponent implements OnInit {
   cadastro: FormGroup;
   generos : Array<string>;
 
+
   constructor(public validacao: ValidarCamposService,
-              private fb: FormBuilder) { }
+              public dialog: MatDialog,
+              private fb: FormBuilder,
+              private filmeService: FilmesService,
+              private router: Router
+              ) { }
 
   ngOnInit() {
 
@@ -32,21 +43,63 @@ export class CadastroFilmesComponent implements OnInit {
   }
 
   reiniciarForm(): void {
-    this.cadastro.reset;
+    this.cadastro.reset();
   }
 
-  salvar(): void {
+  submit(): void {
     this.cadastro.markAllAsTouched();
     if(this.cadastro.invalid){
       return;
     }
     else{
-      alert('Sucesso\n ' + JSON.stringify(this.cadastro.value, null,4));
+       const filme = this.cadastro.getRawValue() as Filme;
+       this.salvar(filme);
     }
   }
 
   get f(){
     return this.cadastro.controls;
+  }
+
+  private salvar(filme : Filme): void {
+     this.filmeService.salvar(filme).subscribe( () => {
+      const config = {
+        data: {
+          btnSucesso: 'Ir para a listagem',
+          btnCancelar: 'Cadastar um novo filme',
+          corBtnCancelar: 'primary',
+          possuirBtnFechar: true,
+
+
+        } as Alerta
+      };
+
+      const dialogRef = this.dialog.open(AlertaComponent, config);
+
+      dialogRef.afterClosed().subscribe((opcao: boolean) => {
+        if(opcao){
+          this.router.navigateByUrl('filmes');
+        }
+        else{
+          this.reiniciarForm();
+        }
+      })
+
+     },
+     () => {
+      const config = {
+        data: {
+          btnSucesso: 'Fechar',
+          titulo: 'Erro ao salvar o registro',
+          descricao: 'Não foi possível salvar o registro',
+          corBtnSucesso: 'warn'
+        } as Alerta
+      };
+
+      this.dialog.open(AlertaComponent, config);
+     }
+
+     );
   }
 
 
---
+++ src/app/shared/models/alerta.ts
@@ -0,0 +1,9 @@
+export interface Alerta {
+  titulo  ?: string;
+  descricao ?: string;
+  btnSucesso ?: string;
+  btnCancelar ?: string;
+  possuirBtnFechar ?: boolean;
+  corBtnSucesso ?: string;
+  corBtnCancelar ?: string;
+}
